5d35fbe2dd9d4cdec7aede1490992d634cf157ce
Stas <stastolsto@gmail.com>
2020-07-20 18:47:27 +0300

    Add modules

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'eo1yKbgCtI60iJ6jdXAGsjCzxu6yKWCU',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,20 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
+            'rules' => array(
+                ''=>'site/inex',
+                '<action>'=>'site/<action>',
+            ),
         ],
-        */
+    ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
     ],
     'params' => $params,
 ];
